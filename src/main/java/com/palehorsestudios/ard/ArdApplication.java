0d48cb5f273e4ad130525923cea403cde2ca8376
Chase Tucker <tucchase@amazon.com>
2020-09-10 17:30:39 -0700

    spring stubbed out

---
+++ src/main/java/com/palehorsestudios/ard/ArdApplication.java
@@ -0,0 +1,13 @@
+package com.palehorsestudios.ard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/palehorsestudios/ard/ArdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palehorsestudios.ard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
